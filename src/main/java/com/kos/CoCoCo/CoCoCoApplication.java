92beff4031cfb844c18b00232d7ee2703cf5f891
banggu0321 <99bang@naver.com>
2022-07-13 14:50:42 +0900

    create project

---
+++ src/main/java/com/kos/CoCoCo/CoCoCoApplication.java
@@ -0,0 +1,13 @@
+package com.kos.CoCoCo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoCoCoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoCoCoApplication.class, args);
+	}
+
+}

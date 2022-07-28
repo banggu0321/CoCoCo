9dd9d9b3c7895d8f2d14e1b8515c9cf59b34ff6d
banggu0321 <99bang@naver.com>
2022-07-28 16:03:18 +0900

    update/delete work(post,put,delete error)

--- src/main/java/com/kos/CoCoCo/gy/controller/GyWorkRestController.java
+++ src/main/java/com/kos/CoCoCo/gy/controller/GyWorkRestController.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -131,5 +132,10 @@ public class GyWorkRestController {
 			System.out.println("Manager 변동없음");
 		}
 		return work;
+	}
+	@DeleteMapping(value="/workdetail.go/{work_id}")
+	public void deleteWork(@PathVariable Long work_id) {
+		workManagerRepo.workManagerDelete(work_id);
+		workRepo.deleteById(work_id);
 	}
 }

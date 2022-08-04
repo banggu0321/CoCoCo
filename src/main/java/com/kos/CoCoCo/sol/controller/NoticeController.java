c7b82bae14ac2a10ea36671213525ab9769df9b9
pineyun <findfinepine@gmail.com>
2022-08-04 16:59:33 +0900

    sol commit

--- src/main/java/com/kos/CoCoCo/sol/controller/NoticeController.java
+++ src/main/java/com/kos/CoCoCo/sol/controller/NoticeController.java
@@ -12,21 +12,18 @@ import javax.servlet.http.HttpSession;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.support.PagedListHolder;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.core.io.Resource;
+
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
+
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -100,7 +97,7 @@ public class NoticeController {
 	
 	
 	@GetMapping("/detail")
-	public String detail(NoticePageVO pageVO, NoticeFile noticefile, NoticeVO notice, Long noticeId, HttpSession session, Model model) {
+	public String detail(NoticePageVO pageVO, NoticeVO notice, Long noticeId, Model model) {
 		
 		log.info("공지번호 : " + noticeId);
 		
@@ -110,9 +107,7 @@ public class NoticeController {
 		List<NoticeFile> nflist = noticeFRepo.findByNotice(notice);
 		System.out.println("nflist : " + nflist);
 		model.addAttribute("nflist", nflist);
-		if(nflist != null) {
-			
-		}
+		
 		
 		return "notice/noticedetail";
 	}

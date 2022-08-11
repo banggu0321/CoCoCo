5770a8e14dd289f98dc874a89e69ed94e76f1d5f
jaeyong <lkmdl@gmail.com>
2022-08-11 22:30:46 +0900

    addd

--- src/main/java/com/kos/CoCoCo/choi/UserControllerC.java
+++ src/main/java/com/kos/CoCoCo/choi/UserControllerC.java
@@ -13,21 +13,30 @@ import com.kos.CoCoCo.vo.UserVO;
 @Controller
 public class UserControllerC {
 
-    @Autowired
-    @Qualifier("userService")
-    private UserServiceC userService;
+	@Autowired
+	@Qualifier("userService")
+	private UserServiceC userService;
 
-    
-    // 아이디 체크
-    @PostMapping("/auth/idCheck")
-    @ResponseBody
-    public int idCheck(@RequestParam("userId") String userId){
-    	UserVO user = userService.idCheck(userId);
-    	System.out.println(user);
-    	return user==null?0:1;
+	// 아이디 체크
+	@PostMapping("/auth/idCheck")
+	@ResponseBody
+	public int idCheck(@RequestParam("userId") String userId) {
+		UserVO user = userService.idCheck(userId);
+		System.out.println(user);
+		return user == null ? 0 : 1;
 
-    }
-   
+	}
+
+// 비밀번호 체크
+	@PostMapping("/auth/pwCheck")
+	@ResponseBody
+	public int pwCheck(@RequestParam("pw") String pw) {
+		UserVO user = userService.pwCheck(pw);
+		System.out.println(user);
+		return user == null ? 0 : 1;
+
+	}
+
 }
 //1.Controller ->2.Service ->3.Repository ->4.Mapper
 //5.Mapper ->6.Repository ->7.Service  ->8.Controller
--- src/main/java/com/kos/CoCoCo/choi/UserServiceImpl.java
+++ src/main/java/com/kos/CoCoCo/choi/UserServiceImpl.java
@@ -7,16 +7,23 @@ import org.springframework.stereotype.Service;
 import com.kos.CoCoCo.vo.UserVO;
 
 @Service("userService")
-public class UserServiceImpl implements UserServiceC{
+public class UserServiceImpl implements UserServiceC {
 
-    @Autowired
-    private UserRepositoryC repo;
+	@Autowired
+	private UserRepositoryC repo;
 
-    // 아이디 중복 체크
-    @Override
+	// 아이디 중복 체크
+	@Override
     public UserVO idCheck(String id) {
        UserVO user = repo.findById(id).orElse(null);
         return user;
-    }
+	}
+        
+    // 비밀번호 중복 체크
+	@Override
+	public UserVO pwCheck(String pw) {
+		UserVO user = repo.findById(pw).orElse(null);
+		return user;
+	}
 
 }
